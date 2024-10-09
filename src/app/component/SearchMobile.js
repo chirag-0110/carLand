349dbf90c98d7d0d5228a270af92c294c4edd12f
chirag <chiragsharma1676@gmail.com>
2024-10-09 16:38:38 +0530

    added date, hour and location selection components

--- src/app/component/SearchMobile.js
+++ src/app/component/SearchMobile.js
@@ -1,7 +1,21 @@
+import DateSelection from "./DateSelection"
+import HoursSelection from "./HoursSelection"
+import LocationSelection from "./LocationSelection"
 
 const SearchMobile = () => {
   return (
-    <div>searchMobile</div>
+    <div className="xl:hidden font-medium">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-4">
+          <LocationSelection />
+          <DateSelection />
+          <HoursSelection />
+          <div>
+            <button className="btn btn-sm btn-accent w-[164px] mx-auto">Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
